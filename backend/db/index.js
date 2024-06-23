8c75138cd1b530b46425ecd55a74f3f748c4f42f
Vivek Kumar <vivekyadavnitt@gmail.com>
2024-06-24 00:16:00 +0530

    pending change

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 
-// mongoose.connect('mongodb://localhost:27017/CampusBite');
-
-mongoose.connect('mongodb://localhost:27017/Campus_Bite');
+mongoose.connect('mongodb://localhost:27017/Campus_Bite'); //not the right way use .env file
 
 
 //Student Schema
